41f4b44b15071cffaf5b28e1d1b6cc6329a8ceb1
scmedina <92344656+scmedina@users.noreply.github.com>
2023-09-03 10:33:35 -0500

    Added IFilterProperties

---
+++ SeatsAeroLibrary/Helpers/TypeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeatsAeroLibrary.Helpers
+{
+    public class TypeHelper
+    {
+        public static List<Type> GetTypesImplementingInterface<T>()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            return assembly.GetTypes()
+                .Where(type => typeof(T).IsAssignableFrom(type) && type.IsClass)
+                .ToList();
+        }
+    }
+}
---
+++ SeatsAeroLibrary/Services/IFilterProperties.cs
@@ -0,0 +1,22 @@
+﻿using SeatsAeroLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeatsAeroLibrary.Services
+{
+    internal interface IFilterProperties
+    {
+        int SeatsAvailable { get; set; }
+        DateTime StartDate { get; set; }
+        DateTime EndDate { get; set; }
+        SeatType SeatType { get; set; }
+        bool DirectOnly { get; set; }
+        int MaxPoints { get; set; }
+        List<string> StartingAirports { get; set; }
+        List<string> EndingAirports { get; set; }
+
+    }
+}
